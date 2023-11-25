bcedc6746267082598c91920ad536233f0ac0f7d
bobore92 <151954706+bobore92@users.noreply.github.com>
2023-11-25 18:13:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,6 @@ class RenovationAssistant:
     def get_category_advice(self, category):
         return self.supplier_categories.get(category.lower(), "I'm not sure about that category. Can you specify which service you are looking for?")
 
-# Instantiate the assistant class using the OpenAI API key from Streamlit secrets
-assistant = RenovationAssistant(st.secrets["OPENAI_KEY"])
-
 # Streamlit app initialization
 if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = [{
@@ -59,26 +56,27 @@ if 'conversation_history' not in st.session_state:
 
 st.title("AI - House Renovation")
 
+# Instantiate the assistant class using the OpenAI API key from Streamlit secrets
+assistant = RenovationAssistant(st.secrets["OPENAI_KEY"])
+
 # Display the conversation history
-for message in st.session_state.conversation_history:
+for message in reversed(st.session_state.conversation_history[-2:]):
     st.write(f"{message['role'].title()}: {message['content']}")
 
-# User input form
-with st.form(key='user_input_form'):
-    user_input = st.text_input("How may I assist with your home renovation?", key='user_input')
+# User input form to manage the state properly
+with st.sidebar(key='user_input_form'):
+    user_input = st.text_input("")
     form_submit = st.form_submit_button("Submit")
 
 # Callback function to handle form submission
 def handle_form_submission():
     user_question = user_input  # Retrieve the input from the form
     if user_question:
-        # Append the user's question
-        st.session_state.conversation_history.append({'role': 'user', 'content': user_question})
-
         # Process the user's question against the assistant's supplier categories
         category_found = False
         for category, advice in assistant.supplier_categories.items():
             if category in user_question.lower():
+                st.session_state.conversation_history.append({'role': 'user', 'content': user_question})
                 st.session_state.conversation_history.append({'role': 'assistant', 'content': advice})
                 category_found = True
                 break
@@ -86,13 +84,13 @@ def handle_form_submission():
         # If the user's question does not match any category, ask the assistant (GPT-4 model)
         if not category_found:
             answer = assistant.ask_openai(user_question, st.session_state.conversation_history)
+            st.session_state.conversation_history.append({'role': 'user', 'content': user_question})
             st.session_state.conversation_history.append({'role': 'assistant', 'content': answer})
 
-        # The form has been submitted, now display the assistant's response
-        st.write(f"Assistant: {st.session_state.conversation_history[-1]['content']}")
+        # Display the assistant's and user's messages in reverse order (assistant's message on top)
+        for message in reversed(st.session_state.conversation_history[-2:]):
+            st.write(f"{message['role'].title()}: {message['content']}")
 
 # Check if the user input form has been submitted
 if form_submit:
     handle_form_submission()
-
-
