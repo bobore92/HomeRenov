4e08faeccc55a12793c2ad1d876ad1caea08f36a
bobore92 <151954706+bobore92@users.noreply.github.com>
2023-11-25 14:38:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,73 +3,92 @@ import requests
 import streamlit as st
 
 class RenovationAssistant:
-    # ... [existing class definition] ...
+    def __init__(self, openai_api_key):
+        self.openai_api_key = openai_api_key
+        self.supplier_categories = {
+            'notary': 'Notary services provide witness and legal formalities for documentation.',
+            'tax accountant': 'Tax accountants can help you maximize your returns and manage your financial paperwork.',
+            'architect': 'Architects design the structure and aesthetics of your home according to your vision.',
+            'building company': 'Building companies execute the construction and renovation work on your home.'
+        }
 
+    def ask_openai(self, question, conversation_history):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.openai_api_key}'
+        }
+        data = {
+            'model': 'gpt-4-1106-preview',
+            'messages': conversation_history + [{'role': 'user', 'content': question}]
+        }
+        response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=data)
+        if response.status_code == 200:
+            answer_content = response.json()['choices'][0]['message']['content']
+            return answer_content
+        else:
+            return f"Error: {response.status_code}, {response.text}"
+
+    def get_category_advice(self, category):
+        return self.supplier_categories.get(category.lower(), "I'm not sure about that category. Can you specify which service you are looking for?")
+
+# Initialize the conversation history in the session state
 if 'conversation_history' not in st.session_state:
     st.session_state['conversation_history'] = [{
-        'id': 'welcome',
         'role': 'system',
-        'content': (
-            "As a Home Renovation Project Assistant, I am your indispensable guide "
-            "throughout every phase of the renovation journey."
-        )
+        'content': "As a Home Renovation Project Assistant, I am your indispensable guide " \
+                   "throughout every phase of the renovation journey. From the initial concept " \
+                   "to the final finishing touches, I seamlessly integrate into your project, " \
+                   "ensuring a smooth and stress-free experience."
     }]
 
-# Function that generates a unique identifier for a message
-def generate_message_id():
-    return str(hash(f"{st.session_state['conversation_history'][-1]['id']}:{len(st.session_state['conversation_history'])}"))
-
+# Title of the Streamlit app
 st.title("Home Renovation Assistant")
 
-# Instantiate the assistant class
+# Instantiate the RenovationAssistant class with the OpenAI API key from the environment variable
 assistant = RenovationAssistant(st.secrets["OPENAI_KEY"])
 
-# Display the existing conversation history
-for message in st.session_state['conversation_history']:
+# Display all messages in the conversation history
+for message in st.session_state.conversation_history:
+    # Every message is displayed with the role as a title
     st.write(f"{message['role'].title()}: {message['content']}")
 
-# User input
-user_input = st.text_input("How may I assist with your home renovation?", key="user_input")
+# Input text box for user query
+user_question = st.text_input("How may I assist with your home renovation?", key="user_input")
 
-# On 'Submit' button click
+# Process the input on pressing the 'Submit' button
 if st.button("Submit"):
-    # Append the user question to the conversation history only if it is not the last one
-    if user_input and (not st.session_state['conversation_history'] or 
-                       st.session_state['conversation_history'][-1].get('content') != user_input):
-        # Add the user question with a unique ID
+    # Ensure no duplicate messages by checking the last message content
+    if user_question \
+       and (not st.session_state['conversation_history'] or
+            st.session_state['conversation_history'][-1]['content'] != user_question):
+        # Add the user's message to the conversation history
         st.session_state['conversation_history'].append({
-            'id': generate_message_id(),
             'role': 'user',
-            'content': user_input
+            'content': user_question
         })
-        
+        # Find if the user's message matches a supplier category
         category_found = False
         for category, advice in assistant.supplier_categories.items():
-            if category in user_input.lower():
+            if category in user_question.lower():
+                # Add the assistant's reply to the conversation history
                 st.session_state['conversation_history'].append({
-                    'id': generate_message_id(),
                     'role': 'assistant',
                     'content': advice
                 })
                 category_found = True
-                break
-
-        # If no category found ask OpenAI API
+                break  # Exit the loop on the first match
+        
+        # Ask OpenAI if no categorized advice was found
         if not category_found:
-            answer = assistant.ask_openai(user_input, st.session_state['conversation_history'])
+            # Get the answer from OpenAI and add to the conversation history
+            answer = assistant.ask_openai(user_question, st.session_state['conversation_history'])
             st.session_state['conversation_history'].append({
-                'id': generate_message_id(),
                 'role': 'assistant',
                 'content': answer
             })
 
-        # Clear the input
+        # Clear the user input field for next message
         st.session_state['user_input'] = ''
-        # Rerun to refresh the state and the display
+        # Rerun the app to clear the input box and avoid duplicated processing
         st.experimental_rerun()
-
-# Always show the last message
-if st.session_state['conversation_history']:
-    last_msg = st.session_state['conversation_history'][-1]
-    st.write(f"{last_msg['role'].title()}: {last_msg['content']}")
 
